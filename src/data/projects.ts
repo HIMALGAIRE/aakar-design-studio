e93f88745c8277d3b3d3eba04ee24b571921426a
Himal Gaire <gyanugaire999@gmail.com>
2026-06-07 01:48:57 +0545

    hey

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,7 +16,7 @@ export const projectsData: Project[] = [
     category: "Residential",
     location: "Tilottama, Nepal",
     structure: "Exposed Concrete & Steel",
-    image: "/src/assets/images/side view.jpeg",
+    image: "/images/side view.jpeg",
     featured: true,
     size: "large"
   },
@@ -26,7 +26,7 @@ export const projectsData: Project[] = [
     category: "Residential",
     location: "Tilottama-5, Manigram",
     structure: "Glass, Timber & Stone",
-    image: "/src/assets/images/pro_1.jpeg",
+    image: "/images/pro_1.jpeg",
     featured: true,
     size: "large"
   },
