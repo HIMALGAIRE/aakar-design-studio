63df9cf943dfe0167f48b99f820a6bd2d449b692
Himal Gaire <gyanugaire999@gmail.com>
2026-06-07 08:35:56 +0545

    tttt

--- src/components/Staff.tsx
+++ src/components/Staff.tsx
@@ -23,7 +23,7 @@ const staffMembers: StaffMember[] = [
     id: 1,
     name: "Ar. Ishmriti Gaire",
     role: "Architect / Founder",
-    image: "image/ishmriti.jpeg",
+    image: "./public/image/ishmriti.jpeg",
     coordinates: "DEG.01 / ARCH_FOUND",
     socials: {
       linkedin: "https://www.linkedin.com/in/ishmriti-gaire-b69466329",
